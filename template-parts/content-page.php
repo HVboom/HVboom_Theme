effe99c2468b74cc61ecb0d6efa33c2021d76bbe
Mario Lotz <Mario.Lotz@HVboom.ch>
2015-05-27 18:54:46 +0200

    Enable responsive images

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -8,20 +8,26 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_title('<h1 class="entry-title">', '</h1>'); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+		<?php if (has_post_thumbnail()) : ?>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+        <?php the_post_thumbnail('thumbnail', array('class' => 'img-responsive')); ?>
+			</a>
+		<?php endif; ?>
+
 		<?php the_content(); ?>
 		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'hvboom' ),
+			wp_link_pages(array(
+				'before' => '<div class="page-links">' . esc_html__('Pages:', 'hvboom'),
 				'after'  => '</div>',
-			) );
+			));
 		?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php edit_post_link( esc_html__( 'Edit', 'hvboom' ), '<span class="edit-link">', '</span>' ); ?>
+		<?php edit_post_link(esc_html__('Edit', 'hvboom'), '<span class="edit-link">', '</span>'); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -6,7 +6,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_title('<h1 class="entry-title">', '</h1>'); ?>
 
 		<div class="entry-meta">
 			<?php hvboom_posted_on(); ?>
@@ -14,12 +14,16 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+		<?php if (has_post_thumbnail()) : ?>
+			<?php the_post_thumbnail('thumbnail', array('class' => 'img-responsive')); ?>
+		<?php endif; ?>
+
 		<?php the_content(); ?>
 		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'hvboom' ),
+			wp_link_pages(array(
+				'before' => '<div class="page-links">' . esc_html__('Pages:', 'hvboom'),
 				'after'  => '</div>',
-			) );
+			));
 		?>
 	</div><!-- .entry-content -->
 
